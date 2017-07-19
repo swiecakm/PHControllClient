acf062cef0f8f67c2a69c3aa3938f89a5d111cbf
Michal Swiecak <swiecak.m@gmail.com>
2017-07-20 00:37:25 +0200

    removing no parameters constructors from my exceptions

--- app/src/main/java/phcontroll/com/phcontrollclient/NetClientBroadcastException.java
+++ app/src/main/java/phcontroll/com/phcontrollclient/NetClientBroadcastException.java
@@ -3,9 +3,6 @@ package phcontroll.com.phcontrollclient;
 public class NetClientBroadcastException extends Exception {
     private static final long serialVersionUID = -3279582061542873813L;
 
-    public NetClientBroadcastException() {
-    }
-
     public NetClientBroadcastException(String message) {
         super(message);
     }
--- app/src/main/java/phcontroll/com/phcontrollclient/NetClientServerResponseException.java
+++ app/src/main/java/phcontroll/com/phcontrollclient/NetClientServerResponseException.java
@@ -3,9 +3,6 @@ package phcontroll.com.phcontrollclient;
 public class NetClientServerResponseException extends Exception {
     private static final long serialVersionUID = -1911829663171570739L;
 
-    public NetClientServerResponseException() {
-    }
-
     public NetClientServerResponseException(String message) {
         super(message);
     }
